0266901a19d84659b998a02865609cd792ef30ff
ברק <barakyb89@gmail.com>
2022-05-22 10:27:51 +0300

    ork

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import SignUpUser from "./Component/SignUp";
+import Demo from './Demo';
+import PrimarySearchAppBar from "./Component/AppBar";
+import { Routes , Route} from 'react-router-dom';
+import StaticTimePicker from "./Component/StaticTimePicker";
+
+function App() {
+    return (
+      <div className={"App"}>
+      <PrimarySearchAppBar/>
+        <SignUpUser/>
+      <Routes>
+          <Route path={'/'} component={<SignUpUser/>} />
+          <Route path={'/demo'} component={<Demo/>}/>
+          <Route path={'/staticTimePicker'} component={<StaticTimePicker/>}/>
+
+      </Routes>
+      </div>
+    );
+}
+
+export default  App;
